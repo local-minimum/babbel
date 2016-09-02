78423bfaf6dd8f0ebc3c4d2e994d552c06e78476
Martin Zackrisson <martin.zackrisson@gmail.com>
2016-09-02 19:57:20 +0200

    Modal buttons

--- Assets/Addons/Babbel/Scripts/EditorWindows/TagsWindow.cs
+++ Assets/Addons/Babbel/Scripts/EditorWindows/TagsWindow.cs
@@ -3,19 +3,60 @@ using UnityEditor;
 using System.Collections;
 
 namespace Babbel {
+
 	public class TagsWindow : EditorWindow {
 
-		[MenuItem("Window/Babbel/Tags...")]
-		public static void ShowWindow() {
+        enum DisplayMode { All, Ranked, Used, NotUsed};
 
+        string filterQuery;
+        DisplayMode displayMode = DisplayMode.All;
+
+        public static GUIStyle inactiveToggleButtons = new GUIStyle();
+        public static GUIStyle activeToggleButtons = new GUIStyle();
+
+        GUIContent allMode = new GUIContent("All", "All registered tags");
+        GUIContent rankedMode = new GUIContent("Ranked", "Sort tags based on usage");
+        GUIContent usedMode = new GUIContent("Used", "Only tags in use");
+        GUIContent notUsedMode = new GUIContent("Not Used", "Only tags never used");
+
+        [MenuItem("Window/Babbel/Tags...")]
+		public static void ShowWindow() {
 
 			EditorWindow window = EditorWindow.GetWindow (typeof(TagsWindow));
 			window.titleContent = new GUIContent ("Babbel:Tags");
 		}
 
 		void OnGUI() {
+
+            AddModalButtons();
+
 			// Show all the tags...
 		}
+
+        void AddModalButtons()
+        {
+            //Modal buttons
+            GUILayout.BeginHorizontal();
+            LayOutModal(DisplayMode.All, allMode);
+            LayOutModal(DisplayMode.Ranked, rankedMode);
+            LayOutModal(DisplayMode.Used, usedMode);
+            LayOutModal(DisplayMode.NotUsed, notUsedMode);
+            GUILayout.EndHorizontal();
+        }
+
+        void LayOutModal(DisplayMode mode, GUIContent content)
+        {
+            if (mode == displayMode)
+            {
+                GUILayout.Label(content);
+            } else
+            {
+                if (GUILayout.Button(content))
+                {
+                    displayMode = mode;
+                }
+            }
+        }
 	}
 
 }
